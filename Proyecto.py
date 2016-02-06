9d30229febc42f6167d768cc723778690302c429
Pablo Betancourt <pablodbc30@mail.com>
2016-02-06 18:57:21 -0430

    El quicksort de Palma ya sirve

--- Proyecto.py
+++ Proyecto.py
@@ -1,5 +1,12 @@
 from math import*
 from random import*
+#estaOrdenado
+def esta_ordenado(a):
+	k = all(a[i]<=a[i+1] for i in range(len(a)-1))
+	if k: print("Esta ordenado")
+	else: print("Jodete")
+##########################################################################################################################################
+
 #Insertion sort
 def insertionsort(A,p,r):
 	for i in range(p+1,r+1):
@@ -63,21 +70,19 @@ def median_of_three(a,b,c):
 	B.sort()
 	return B[1]
 ##########################################################################################################################################
-
 def quicksort(A,p,r):
+	quicksort_loop(A,p,r)
+	insertionsort(A,p,r)
+def quicksort_loop(A,p,r):
 	while r-p+1>15:
 		m = Partition(A,p,r,median_of_three(A[p],A[r],A[(p+r)//2]))
 		if m-p >= r-m:
-			quicksort(A,m,r)
+			quicksort_loop(A,m,r)
 			r = m
 		else:
-			quicksort(A,p,m)
-			p = m
-	#insertionsort(A,p,r)
+			quicksort_loop(A,p,m)
+			p = m+1
 ############################################################################################################################################
-a = [1 for i in range(50)]
-a[0] = a[10] = 2
-print(a)
-quicksort(a,0,len(a)-1)
-#Partition(a,0,len(a)-1,1)
-print(a)
+a = [randint(0,10) for i in range(4000)]
+insertionsort(a,0,len(a)-1)
+esta_ordenado(a)
